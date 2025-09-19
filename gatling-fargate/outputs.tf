6e4d979bbe629ae8e09d5e6544fdac88d6a0ec73
Raghul <rmurugesan@adaptavist.com>
2025-09-19 14:23:27 -0400

    trigger pipeline

--- gatling-fargate/outputs.tf
+++ gatling-fargate/outputs.tf
@@ -2,10 +2,6 @@ output "cluster_name" {
   value = aws_ecs_cluster.this.name
 }
 
-output "cluster_id" {
-  value = aws_ecs_cluster.this.id
-}
-
 output "fargate_vpc_id" {
   value = aws_security_group.fargate.vpc_id
 }
