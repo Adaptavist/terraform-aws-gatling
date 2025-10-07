562c4e898d8b8b5064a8dce95de878e9f84692ee
Raghul <rmurugesan@adaptavist.com>
2025-10-07 12:45:49 -0400

    fix:(dependency cycle)

--- gatling-fargate/main.tf
+++ gatling-fargate/main.tf
@@ -26,7 +26,7 @@ module "ecs-container-definition" {
     { name = "SERVICE_NAME", value = var.target_service },
     { name = "SIM_CLASS", value = var.sim_class },
     { name = "AWS_REGION", value = var.region },
-    { name = "CLUSTER", value = aws_ecs_cluster.this.name },
+    { name = "CLUSTER", value = var.service_name },
     { name = "TASK_DEF", value = aws_ecs_task_definition.fargate.family },
     { name = "SECURITY_GROUP", value = aws_security_group.fargate.id },
     { name = "SUBNETS", value = var.subnet_ids }
